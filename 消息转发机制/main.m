63ef723ea600a18911ea4d5330e0c9c53163f00b
张岩 <zy@zydemacbook-pro.local>
2016-05-04 00:06:17 +0800

    Initial Commit

---
+++ 消息转发机制/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  消息转发机制
+//
+//  Created by zy on 16/5/4.
+//  Copyright © 2016年 ZY. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
